2fb759ff1f5a1c7180f4c63f3b5ceca5a9745850
Nitya Subramanian <nityas@mit.edu>
2014-11-16 17:59:33 -0500

    mast skeleton

--- src/mast/mast.py
+++ src/mast/mast.py
@@ -1,13 +1,15 @@
 from crypto import *
 class Mast():
 
-	def __init__(self, mode, content, honest=True, addNonces=True, debug=True, leaf=False):
-		modes = ["client","server"]
+	def __init__(self, mode, content, parent=None, honest=True, addNonces=True, debug=True, leaf=False):
+		modes = ["run","compile"]
 		if mode not in modes:
 			raise ValueError("Mode not in %s "%modes)
 		self.mode = mode
 		self.content = content
-		self.contenthash = _hash(content)	#TODO: syntax check content
+		if self.mode =="compile":
+			self.contenthash = _hash(content)	#TODO: syntax check content
+		self.parent = parent
 		self.honest = honest
 		self.addNonces = addNonces
 		self.debug = debug
@@ -15,22 +17,30 @@ class Mast():
 		self.leaf = leaf
 
 	#TODO: return new node
-	def addBr(self, content, leaf=False):	
-		if self.leaf:
-			raise ValueError("Leaf node cannot have children")
-		if isInstance(content, Mast):
-			pass	#TODO
-		elif isInstance(content, str):
-			pass 	#TODO
-		else:
-			raise ValueError("Illegal content type: %s"%str(content))
+	def addBr(self, content, hash=None, leaf=False):
+		newBr = None	#create new mast
+		if self.mode == "compile":
+			if self.leaf:
+				raise ValueError("Leaf node cannot have children")
+			
+			if isInstance(content, Mast):
+				pass	#TODO
+			elif isInstance(content, str):
+				pass 	#TODO
+			else:
+				raise ValueError("Illegal content type: %s"%str(content))
+			return newBr
+
+		else:	#TODO: All different	
+			return newBr
 
 	#TODO: make this pretty
 	def __str__(self):
 		pass
 
-	def run(self, port=8000, host="localhost"):
-		if self.mode == "client":
+	#TODO: this will be moved
+	def construct(self, port=8000, host="localhost"):
+		if self.mode == "run":
 			pass	#TODO
 		else:
 			pass	#TODO

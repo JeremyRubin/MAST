8f5fa35834652d40ffeb19dfe0503c88e57210c1
Nitya Subramanian <nityas@mit.edu>
2014-11-30 10:34:59 -0500

    added server side mast execution

--- mast/server.py
+++ mast/server.py
@@ -7,6 +7,7 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def initialize(self, mast):
         self._mast = mast
+        self.__IO__ = __IO__()
 
     def get(self):
         self.write("GET uri: %s" % self.request.uri)
@@ -26,6 +27,14 @@ class MastServer():
     def start(self):
         self._application.listen(self._port)
         tornado.ioloop.IOLoop.instance().start()
+
+    def get_req(self, data):
+        self.__IO__.push(data)
+
+    def exec_on_merkle(self, fun, *args):
+        #to exec functions on self._mast
+        #Usage: exec_on_merkle(fn_name, arg1, arg2, etc)
+        self._mast.fun(*args)
 
 if __name__ == "__main__":
     # Usage: python server.py [port]

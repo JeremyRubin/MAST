53d45d371d22ea6427ef3b02498e301505e69c29
Nitya Subramanian <nityas@mit.edu>
2014-12-02 16:10:06 -0500

    slight refactor, added io module

--- mast/mast.py
+++ mast/mast.py
@@ -1,5 +1,7 @@
 from crypto import *
 import crypto
+import io
+from io import *
 class Mast():
 
     def __init__(self, mode, content, parent=None, honest=True, addNonces=False, debug=True, leaf=False):
@@ -123,31 +125,6 @@ def prove(proofList, data, mroot):
         lastHash = crypto.hashable(c1+c2).hash()
     return lastHash == mroot
 
-class __IO__():
-    def __init__(self):
-        self.history = []
-        self.stack = []
-        self.returnstack = []
-        self.heap = {}
-    def push(self, x):
-        self.stack.append(x)
-        self.history.append(x)
-    def pop(self):
-        return self.stack.pop()
-    def peek(self):
-        return self.stack[-1]
-    def dup(self):
-        self.push(self.peek())
-    def setReturn(self, x):
-        self.returnstack.push(x)
-    def getReturn(self):
-        return self.returnstack.pop()
-    def __str__(self):
-        return """\nStack (Top->Bottom): %s\n
-                Heap: %s \n
-                Execution History (Top->Bottom): %s\n"""%(", ".join(str(x) for x in self.stack[::-1]), 
-                    self.heap, 
-                    ", ".join(str(x) for x in self.history[::-1]))
 
 class __Content__():
     """

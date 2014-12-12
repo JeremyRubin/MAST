59076d706a3e3cf03d45f14adb0f932de3cdc499
Jeremy Rubin <jeremy.l.rubin@gmail.com>
2014-12-12 04:41:19 -0500

    bigcode blockchain compatible tests

--- bin/bigcode.py
+++ bin/bigcode.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 import mast.mast as mast
 import mast.crypto as crypto
+import mast.script as script
 import decimal as dec
 from pprint import pprint as pretty
+
+from base64 import b64encode as enc
 ex = """
 def testfn(x, y, z):
     if x > y:
@@ -18,11 +21,17 @@ if __name__ == "__main__":
     print
     print "Generating Hash"
     mh = m.hash()
-    print "Hash is", mh
+    print "Hash is", enc(mh)
     print "Generating Proof"
     proof = bot.generateFullProofUpward(mh)
     print "proof is:"
     pretty(proof)
+
+    code2, _script = mast.toScript(proof, m.hash())
+    together = enc("".join(map(chr,code2+_script)))
+    print "Blockchain compatible Proof is:"
+    print together
+
 
     c = crypto.hashable(ex%60)
     dec.getcontext().prec = 30
@@ -33,7 +42,14 @@ if __name__ == "__main__":
     rate = dec.Decimal(1) - (compressed_size / initial_size)
     print rate
     print "%s %%" %rate
-    assert mast.upwardProve(proof, mh)
+
+    rate = dec.Decimal(1) - (dec.Decimal(len(together)) / initial_size)
+    print rate
+    print "%s %%" %rate
+    raw_input("Continue:")
+    assert mast.upwardProve(proof, mh, True)
+    raw_input("Continue:")
+    script.run("".join(map(chr, code2+_script)))
 
 
 
